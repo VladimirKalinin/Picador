5e89b9f21bffb28c672a9bacb55cba80bfad7593
VladimirKalinin <vovakalinin1997@gmail.com>
2017-10-17 16:24:38 +0300

    change r2c to c2c

--- Picador/PSTD/Source.cpp
+++ Picador/PSTD/Source.cpp
@@ -36,6 +36,22 @@ public:
     val[1] = Im;
   }
 
+  void set(double Re, double Im)
+  {
+    val[0] = Re;
+    val[1] = Im;
+  }
+
+  void setRe(double Re)
+  {
+    val[0] = Re;
+  }
+
+  void getIm(double Im)
+  {
+    val[1] = Im;
+  }
+
   double getRe()
   {
     return val[0];
@@ -87,7 +103,7 @@ int main() {
   double Z_Min = -0.01;
   double Z_Max = 0.01;
 
-  t = 1000;
+  t = 2000;
   n = 128;
   m = 32;
   h = 32;
@@ -106,12 +122,13 @@ int main() {
   //  fin >> m; 
   //  fin >> h; 
 
-  double *Ex = new double[n*m*h];
-  double *Ey = new double[n*m*h];
-  double *Ez = new double[n*m*h];
-  double *Bx = new double[n*m*h];
-  double *By = new double[n*m*h];
-  double *Bz = new double[n*m*h];
+  myComplex *Ey = new myComplex[n*m*h];
+  myComplex *Ez = new myComplex[n*m*h];
+  myComplex *Bx = new myComplex[n*m*h];
+  myComplex *By = new myComplex[n*m*h];
+  myComplex *Ex = new myComplex[n*m*h];
+  myComplex *Bz = new myComplex[n*m*h];
+
 
   for (i = 0; i < n; i++)
   {
@@ -122,17 +139,17 @@ int main() {
         x = X_Min + i*dx;
         y = Y_Min + j*dy;
         z = Z_Min + k*dz;
-        Ex[i + j*n + k*n*m] = 0;
-        Ey[i + j*n + k*n*m] = ((((exp(((((-((y * y) + (z * z))) * 2) * 0.69314718055994529) / 0.000025000000000000001)))) *
+        Ex[k + h*(j + m*i)].set(0, 0);
+        Ey[k + h*(j + m*i)].set(((((exp(((((-((y * y) + (z * z))) * 2) * 0.69314718055994529) / 0.000025000000000000001)))) *
           (((exp((((-((((2 * x) / (-0.030000000000000002))) * (((2 * x) / (-0.030000000000000002))))) * 2) * 0.69314718055994529)) *
-          (((0.5 * (1 + sign(((-x) - (-0.030000000000000002))))))))))) * cos((2617.9938779914942 * x)));
-        Ez[i + j*n + k*n*m] = 0;
+          (((0.5 * (1 + sign(((-x) - (-0.030000000000000002))))))))))) * cos((2617.9938779914942 * x))), 0);
+        Ez[k + h*(j + m*i)].set(0, 0);
 
-        Bx[i + j*n + k*n*m] = 0;
-        By[i + j*n + k*n*m] = 0;
-        Bz[i + j*n + k*n*m] = ((((exp(((((-((y * y) + (z * z))) * 2) * 0.69314718055994529) / 0.000025000000000000001)))) *
+        Bx[k + h*(j + m*i)].set(0, 0);
+        By[k + h*(j + m*i)].set(0, 0);
+        Bz[k + h*(j + m*i)].set(((((exp(((((-((y * y) + (z * z))) * 2) * 0.69314718055994529) / 0.000025000000000000001)))) *
           (((exp((((-((((2 * x) / (-0.030000000000000002))) * (((2 * x) / (-0.030000000000000002))))) * 2) * 0.69314718055994529)) *
-          (((0.5 * (1 + sign(((-x) - (-0.030000000000000002))))))))))) * cos((2617.9938779914942 * x)));
+          (((0.5 * (1 + sign(((-x) - (-0.030000000000000002))))))))))) * cos((2617.9938779914942 * x))), 0);
       }
     }
   }
@@ -147,12 +164,12 @@ int main() {
 
 
 
-  fftw_plan pEx = fftw_plan_dft_r2c_3d(n, m, h, (double *)Ex, (fftw_complex *)Ex_c, FFTW_MEASURE);
-  fftw_plan pEy = fftw_plan_dft_r2c_3d(n, m, h, (double *)Ey, (fftw_complex *)Ey_c, FFTW_MEASURE);
-  fftw_plan pEz = fftw_plan_dft_r2c_3d(n, m, h, (double *)Ez, (fftw_complex *)Ez_c, FFTW_MEASURE);
-  fftw_plan pBx = fftw_plan_dft_r2c_3d(n, m, h, (double *)Bx, (fftw_complex *)Bx_c, FFTW_MEASURE);
-  fftw_plan pBy = fftw_plan_dft_r2c_3d(n, m, h, (double *)By, (fftw_complex *)By_c, FFTW_MEASURE);
-  fftw_plan pBz = fftw_plan_dft_r2c_3d(n, m, h, (double *)Bz, (fftw_complex *)Bz_c, FFTW_MEASURE);
+  fftw_plan pEx = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ex, (fftw_complex *)Ex_c, FFTW_FORWARD, FFTW_MEASURE);
+  fftw_plan pEy = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ey, (fftw_complex *)Ey_c, FFTW_FORWARD, FFTW_MEASURE);
+  fftw_plan pEz = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ez, (fftw_complex *)Ez_c, FFTW_FORWARD, FFTW_MEASURE);
+  fftw_plan pBx = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Bx, (fftw_complex *)Bx_c, FFTW_FORWARD, FFTW_MEASURE);
+  fftw_plan pBy = fftw_plan_dft_3d(n, m, h, (fftw_complex *)By, (fftw_complex *)By_c, FFTW_FORWARD, FFTW_MEASURE);
+  fftw_plan pBz = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Bz, (fftw_complex *)Bz_c, FFTW_FORWARD, FFTW_MEASURE);
 
   fftw_execute(pEx);
   fftw_execute(pEy);
@@ -160,6 +177,44 @@ int main() {
   fftw_execute(pBx);
   fftw_execute(pBy);
   fftw_execute(pBz);
+
+  fftw_destroy_plan(pEx);
+  fftw_destroy_plan(pEy);
+  fftw_destroy_plan(pEz);
+  fftw_destroy_plan(pBx);
+  fftw_destroy_plan(pBy);
+  fftw_destroy_plan(pBz);
+
+  //double *ex = new double[n*m*h];
+  //double *ey = new double[n*m*h];
+  //double *ez = new double[n*m*h];
+  //double *bx = new double[n*m*h];
+  //double *by = new double[n*m*h];
+  //double *bz = new double[n*m*h];
+
+  pEx = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ex_c, (fftw_complex *)Ex, FFTW_BACKWARD, FFTW_MEASURE);
+  pEy = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ey_c, (fftw_complex *)Ey, FFTW_BACKWARD, FFTW_MEASURE);
+  pEz = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Ez_c, (fftw_complex *)Ez, FFTW_BACKWARD, FFTW_MEASURE);
+  pBx = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Bx_c, (fftw_complex *)Bx, FFTW_BACKWARD, FFTW_MEASURE);
+  pBy = fftw_plan_dft_3d(n, m, h, (fftw_complex *)By_c, (fftw_complex *)By, FFTW_BACKWARD, FFTW_MEASURE);
+  pBz = fftw_plan_dft_3d(n, m, h, (fftw_complex *)Bz_c, (fftw_complex *)Bz, FFTW_BACKWARD, FFTW_MEASURE);
+
+  for (i = 0; i < n; i++)
+  {
+    for (j = 0; j < m; j++)
+    {
+      for (k = 0; k < h; k++)
+      {
+        Ex[k + h*(j + m*i)].set(0, 0);
+        Ey[k + h*(j + m*i)].set(0, 0);
+        Ez[k + h*(j + m*i)].set(0, 0);
+
+        Bx[k + h*(j + m*i)].set(0, 0);
+        By[k + h*(j + m*i)].set(0, 0);
+        Bz[k + h*(j + m*i)].set(0, 0);
+      }
+    }
+  }
 
 
   myComplex myI(0, 1);
@@ -189,9 +244,9 @@ int main() {
       for (j = 0; j < m; j++)
         for (k = 0; k < h; k++)
         {
-          Ex_c[i + j*n + k*n*m] = Ex_c[i + j*n + k*n*m] + myI*(Bz_c[i + j*n + k*n*m] * wy[j] - By_c[i + j*n + k*n*m] * wz[k]);
-          Ey_c[i + j*n + k*n*m] = Ey_c[i + j*n + k*n*m] - myI*(Bz_c[i + j*n + k*n*m] * wx[i] - Bx_c[i + j*n + k*n*m] * wz[k]);
-          Ez_c[i + j*n + k*n*m] = Ez_c[i + j*n + k*n*m] + myI*(By_c[i + j*n + k*n*m] * wx[i] - Bx_c[i + j*n + k*n*m] * wy[j]);
+          Ex_c[k + h*(j + m*i)] = Ex_c[k + h*(j + m*i)] + myI*(Bz_c[k + h*(j + m*i)] * wy[j] - By_c[k + h*(j + m*i)] * wz[k]);
+          Ey_c[k + h*(j + m*i)] = Ey_c[k + h*(j + m*i)] - myI*(Bz_c[k + h*(j + m*i)] * wx[i] - Bx_c[k + h*(j + m*i)] * wz[k]);
+          Ez_c[k + h*(j + m*i)] = Ez_c[k + h*(j + m*i)] + myI*(By_c[k + h*(j + m*i)] * wx[i] - Bx_c[k + h*(j + m*i)] * wy[j]);
         }
 
 
@@ -200,40 +255,43 @@ int main() {
       for (j = 0; j < m; j++)
         for (k = 0; k < h; k++)
         {
-          Bx_c[i + j*n + k*n*m] = Bx_c[i + j*n + k*n*m] - myI*(Ez_c[i + j*n + k*n*m] * wy[j] - Ey_c[i + j*n + k*n*m] * wz[k]);
-          By_c[i + j*n + k*n*m] = By_c[i + j*n + k*n*m] + myI*(Ez_c[i + j*n + k*n*m] * wx[i] - Ex_c[i + j*n + k*n*m] * wz[k]);
-          Bz_c[i + j*n + k*n*m] = Bz_c[i + j*n + k*n*m] - myI*(Ey_c[i + j*n + k*n*m] * wx[i] - Ex_c[i + j*n + k*n*m] * wy[j]);
+          Bx_c[k + h*(j + m*i)] = Bx_c[k + h*(j + m*i)] - myI*(Ez_c[k + h*(j + m*i)] * wy[j] - Ey_c[k + h*(j + m*i)] * wz[k]);
+          By_c[k + h*(j + m*i)] = By_c[k + h*(j + m*i)] + myI*(Ez_c[k + h*(j + m*i)] * wx[i] - Ex_c[k + h*(j + m*i)] * wz[k]);
+          Bz_c[k + h*(j + m*i)] = Bz_c[k + h*(j + m*i)] - myI*(Ey_c[k + h*(j + m*i)] * wx[i] - Ex_c[k + h*(j + m*i)] * wy[j]);
         }
 
-    //if (l % 2 == 0)
-    //{
-    //  string str = "PSTDout"; str += to_string(l); str += ".txt";
-    //  ofstream fout(str);
-    //  fftw_execute(pEx);
-    //  fftw_execute(pEy);
-    //  fftw_execute(pEz);
-    //  fftw_execute(pBx);
-    //  fftw_execute(pBy);
-    //  fftw_execute(pBz);
-    //  double size = n*m*h;
-    //  for (i = 0; i < n; i++) //приводим
-    //    for (j = 0; j < m; j++)
-    //      for (k = 0; k < h; k++)
-    //      {
-    //        Ex[i + j*n + k*n*m] = Ex[i + j*n + k*n*m] / size;
-    //        Ey[i + j*n + k*n*m] = Ey[i + j*n + k*n*m] / size;
-    //        Ey[i + j*n + k*n*m] = Ey[i + j*n + k*n*m] / size;
-    //        Bx[i + j*n + k*n*m] = Bx[i + j*n + k*n*m] / size;
-    //        By[i + j*n + k*n*m] = By[i + j*n + k*n*m] / size;
-    //        By[i + j*n + k*n*m] = By[i + j*n + k*n*m] / size;
-    //      }
-    //  for (i = 0; i < n; i++)
-    //    for (j = 0; j < m; j++)
-    //    {
-    //      fout << Ey[i + j*n + h*n*m / 2] << "\n" << Bz[i + j*n + h*n*m / 2] << "\n";
-    //    }
-    //  fout.close();
-    //}
+    if (l % 64 == 0)
+    {
+      string str = "PSTDout"; str += to_string(l); str += ".txt";
+      ofstream fout(str);
+      fftw_execute(pEx);
+      fftw_execute(pEy);
+      fftw_execute(pEz);
+      fftw_execute(pBx);
+      fftw_execute(pBy);
+      fftw_execute(pBz);
+      //double size = n*m*h;
+      //for (i = 0; i < n; i++) //приводим
+      //  for (j = 0; j < m; j++)
+      //    for (k = 0; k < h; k++)
+      //    {
+      //      Ex[i + j*n + k*n*m] = Ex[i + j*n + k*n*m] / size;
+      //      Ey[i + j*n + k*n*m] = Ey[i + j*n + k*n*m] / size;
+      //      Ey[i + j*n + k*n*m] = Ey[i + j*n + k*n*m] / size;
+      //      Bx[i + j*n + k*n*m] = Bx[i + j*n + k*n*m] / size;
+      //      By[i + j*n + k*n*m] = By[i + j*n + k*n*m] / size;
+      //      By[i + j*n + k*n*m] = By[i + j*n + k*n*m] / size;
+      //    }
+      fout << n << endl;
+      fout << m << endl;
+      fout << h << endl;
+      for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+        {
+          fout << Ey[k + h*(j + m*i)].getRe() << "\n" << Bz[k + h*(j + m*i)].getRe() << "\n";
+        }
+      fout.close();
+    }
   }
 
 
@@ -243,7 +301,6 @@ int main() {
   //fftw_execute(pBx);
   //fftw_execute(pBy);
   //fftw_execute(pBz);
-
 
   //for (i = 0; i < n; i++) //приводим
   //  for (j = 0; j < m; j++)
@@ -273,62 +330,42 @@ int main() {
   //pBy = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)By_c, (double *)By, FFTW_ESTIMATE);
   //pBz = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Bz_c, (double *)Bz, FFTW_ESTIMATE);
 
-  fftw_destroy_plan(pEx);
-  fftw_destroy_plan(pEy);
-  fftw_destroy_plan(pEz);
-  fftw_destroy_plan(pBx);
-  fftw_destroy_plan(pBy);
-  fftw_destroy_plan(pBz);
 
-  double *ex = new double[n*m*h];
-  double *ey = new double[n*m*h];
-  double *ez = new double[n*m*h];
-  double *bx = new double[n*m*h];
-  double *by = new double[n*m*h];
-  double *bz = new double[n*m*h];
-
-  pEx = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Ex_c, (double *)ex, FFTW_MEASURE);
-  pEy = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Ey_c, (double *)ey, FFTW_MEASURE);
-  pEz = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Ez_c, (double *)ez, FFTW_MEASURE);
-  pBx = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Bx_c, (double *)bx, FFTW_MEASURE);
-  pBy = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)By_c, (double *)by, FFTW_MEASURE);
-  pBz = fftw_plan_dft_c2r_3d(n, m, h, (fftw_complex *)Bz_c, (double *)bz, FFTW_MEASURE);
-
-  fftw_execute(pEx);
-  fftw_execute(pEy);
-  fftw_execute(pEz);
-  fftw_execute(pBx);
-  fftw_execute(pBy);
-  fftw_execute(pBz);
+  //fftw_execute(pEx);
+  //fftw_execute(pEy);
+  //fftw_execute(pEz);
+  //fftw_execute(pBx);
+  //fftw_execute(pBy);
+  //fftw_execute(pBz);
 
 
-  for (i = 0; i < n; i++) //приводим
-    for (j = 0; j < m; j++)
-      for (k = 0; k < h; k++)
-      {
+  //for (i = 0; i < n; i++) //приводим
+  //  for (j = 0; j < m; j++)
+  //    for (k = 0; k < h; k++)
+  //    {
 
-        ex[i + j*n + k*n*m] = ex[i + j*n + k*n*m] / n / m / h;
-        ey[i + j*n + k*n*m] = ey[i + j*n + k*n*m] / n / m / h;
-        ey[i + j*n + k*n*m] = ey[i + j*n + k*n*m] / n / m / h;
+  //      Ex[k + h*(j + m*i)] = Ex[k + h*(j + m*i)] / n / m / h;
+  //      Ey[k + h*(j + m*i)] = Ey[k + h*(j + m*i)] / n / m / h;
+  //      Ey[k + h*(j + m*i)] = Ey[k + h*(j + m*i)] / n / m / h;
 
-        bx[i + j*n + k*n*m] = bx[i + j*n + k*n*m] / n / m / h;
-        by[i + j*n + k*n*m] = by[i + j*n + k*n*m] / n / m / h;
-        by[i + j*n + k*n*m] = by[i + j*n + k*n*m] / n / m / h;
-      }
+  //      Bx[k + h*(j + m*i)] = Bx[k + h*(j + m*i)] / n / m / h;
+  //      By[k + h*(j + m*i)] = By[k + h*(j + m*i)] / n / m / h;
+  //      By[k + h*(j + m*i)] = By[k + h*(j + m*i)] / n / m / h;
+  //    }
 
   
 
 
-  ofstream fout("PSTDout.txt");
+ /* ofstream fout("PSTDout.txt");
   fout << n << endl;
   fout << m << endl;
   fout << h << endl;
   for (i = 0; i < n; i++)
     for (j = 0; j < m; j++)
     {
-      fout << ey[i + j*n + h*n*m/2] << "\n" << bz[i + j*n + h*n*m / 2] << "\n";
+      fout << Ey[k + h*(j + m*i)].getRe() << "\n" << Bz[k + h*(j + m*i)].getRe() << "\n";
     }
-  fout.close();
+  fout.close();*/
 //myComplex a(2, 3), b(7, 4), i(0, 1);
 //cout << a.getRe() << " " << a.getIm() << endl;
 //cout << b.getRe() << " " << b.getIm() << endl;
